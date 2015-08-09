e997b98684ff26fb0ef28f8b1da1d2567d9bbbb8
adbid <adam.biddell@gmail.com>
2015-08-09 22:21:59 +0100

    removed fragment

--- app/src/main/java/com/biddster/betterfood/MainActivity.java
+++ app/src/main/java/com/biddster/betterfood/MainActivity.java
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DownloadManager;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,11 +15,9 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.widget.DrawerLayout;
 import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.webkit.JavascriptInterface;
@@ -43,7 +39,6 @@ import static com.biddster.betterfood.Logger.NETWORK;
 import static com.biddster.betterfood.Logger.PREFS;
 import static com.biddster.betterfood.Logger.PRINT;
 import static com.biddster.betterfood.Logger.log;
-import static com.biddster.betterfood.MainActivity.PlaceholderFragment.newInstance;
 
 @SuppressLint({"JavascriptInterface", "AddJavascriptInterface", "SetJavaScriptEnabled"})
 public class MainActivity extends Activity
@@ -59,6 +54,21 @@ public class MainActivity extends Activity
     private static final String goodFoodEvents = "http://www.bbcgoodfood.com/recipes/category/events";
     private static final String goodFoodEveryDay = "http://www.bbcgoodfood.com/recipes/category/everyday";
     private static final String goodFoodIngredients = "http://www.bbcgoodfood.com/recipes/category/ingredients";
+    private final Set<String> allowedHosts = newHashSet("www.bbcgoodfood.com", "ajax.googleapis.com", "code.jquery.com");
+    private final Set<String> ignoredHosts = newHashSet(
+            "d3c3cq33003psk.cloudfront.net",
+            "widget.bbclogin.com",
+            "pq-direct.revsci.net",
+            "cdn.krxd.net",
+            "www.googletagservices.com",
+            "widgets.outbrain.com",
+            "b.scorecardresearch.com",
+            "www.google-analytics.com",
+            "d12au6kkv2cs1v.cloudfront.net",
+            "pagead2.googlesyndication.com",
+            "d2gfdmu30u15x7.cloudfront.net",
+            "js.foodity.com",
+            "api-us1.lift.acquia.com");
 
     private static WebView webView;
     private static ProgressBar progressBar;
@@ -93,11 +103,70 @@ public class MainActivity extends Activity
                 (DrawerLayout) findViewById(R.id.drawer_layout));
 
         mNavigationDrawerFragment.clearDrawer();
+        webView = (WebView)findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new Object() {
+            @JavascriptInterface
+            public void setPrintLink(final String printLink) {
+                log(PRINT, null, "PDF: %s", printLink);
+                MainActivity.printLink = printLink;
+            }
+        }, "PRINTLINK");
+        webView.setWebViewClient(new WebViewClient() {
 
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, newInstance(1))
-                .commit();
+            public WebResourceResponse shouldInterceptRequest(final WebView view, final String url) {
+                try {
+                    final URL aUrl = new URL(url);
+                    if (!ignoredHosts.contains(aUrl.getHost())) {
+                        if (!allowedHosts.contains(aUrl.getHost())) {
+                            log(NETWORK, null, "Loading: %s", aUrl.getHost());
+                        }
+                        return super.shouldInterceptRequest(view, url);
+                    }
+                    return new WebResourceResponse("text/html", "utf-8", null);
+                } catch (final MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+                if (url.startsWith("mailto:")) {
+                    startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse(url)));
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(final WebView view, final String url) {
+                log(NETWORK, null, "Loaded: %s", url);
+                webView.loadUrl("javascript:window.PRINTLINK.setPrintLink(jQuery('.btn-print:first').attr('href'));");
+                webView.loadUrl("javascript:jQuery('.tips-carousel,#buy-ingredients,.side-bar-content,.adsense-ads,#footer').hide()");
+                saveLastPage(url);
+            }
+        });
+        final Animation slideOutTop = AnimationUtils.loadAnimation(this, R.anim.abc_slide_out_top);
+        final Animation slideInTop = AnimationUtils.loadAnimation(this, R.anim.abc_slide_in_top);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(final WebView view, final int progress) {
+                progressBar.setProgress(progress);
+                if (progress == 100) {
+                    progressBar.startAnimation(slideOutTop);
+                    progressBar.setVisibility(View.GONE);
+                } else if (progressBar.getVisibility() == View.GONE) {
+                    progressBar.startAnimation(slideInTop);
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+
+        loadLastPage();
     }
 
     @Override
@@ -136,7 +205,6 @@ public class MainActivity extends Activity
     }
 
     private void goHome() {
-        mNavigationDrawerFragment.clearDrawer();
         webView.loadUrl(goodFoodHome);
     }
 
@@ -150,16 +218,16 @@ public class MainActivity extends Activity
 
     private void saveLastPage(final String url) {
         log(PREFS, null, "Saving page [%s]", url);
-        getSharedPreferences().edit().putString("LastPage", url).apply();
+        getAppSharedPreferences().edit().putString("LastPage", url).apply();
     }
 
     private void loadLastPage() {
-        final String lastPage = getSharedPreferences().getString("LastPage", goodFoodHome);
+        final String lastPage = getAppSharedPreferences().getString("LastPage", goodFoodHome);
         log(PREFS, null, "Loading last page [%s]", lastPage);
         webView.loadUrl(lastPage);
     }
 
-    private SharedPreferences getSharedPreferences() {
+    private SharedPreferences getAppSharedPreferences() {
         return getSharedPreferences("BF", Context.MODE_PRIVATE);
     }
 
@@ -170,7 +238,7 @@ public class MainActivity extends Activity
     }
 
     private void startDownload(final boolean open) {
-        this.open = open;
+        MainActivity.open = open;
         if (!TextUtils.isEmpty(printLink)) {
             final File betterFoodDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "BetterFood");
             betterFoodDirectory.mkdirs();
@@ -191,6 +259,10 @@ public class MainActivity extends Activity
                         Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    private SharedPreferences getSharedPreferences() {
+        return getSharedPreferences("BF", Context.MODE_PRIVATE);
     }
 
     private void viewDownloads() {
@@ -255,38 +327,6 @@ public class MainActivity extends Activity
         }
     }
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.app_name);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 4:
-                mTitle = getString(R.string.title_section3);
-                break;
-            case 5:
-                mTitle = getString(R.string.title_section4);
-                break;
-            case 6:
-                mTitle = getString(R.string.title_section5);
-                break;
-            case 7:
-                mTitle = getString(R.string.title_section6);
-                break;
-            case 8:
-                mTitle = getString(R.string.title_section7);
-                break;
-            case 9:
-                mTitle = getString(R.string.title_section8);
-                break;
-        }
-    }
-
     public void restoreActionBar() {
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
@@ -305,208 +345,5 @@ public class MainActivity extends Activity
             return true;
         }
         return super.onCreateOptionsMenu(menu);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        private final Set<String> allowedHosts = newHashSet("www.bbcgoodfood.com", "ajax.googleapis.com", "code.jquery.com");
-        private final Set<String> ignoredHosts = newHashSet(
-                "d3c3cq33003psk.cloudfront.net",
-                "widget.bbclogin.com",
-                "pq-direct.revsci.net",
-                "cdn.krxd.net",
-                "www.googletagservices.com",
-                "widgets.outbrain.com",
-                "b.scorecardresearch.com",
-                "www.google-analytics.com",
-                "d12au6kkv2cs1v.cloudfront.net",
-                "pagead2.googlesyndication.com",
-                "d2gfdmu30u15x7.cloudfront.net",
-                "js.foodity.com",
-                "api-us1.lift.acquia.com");
-
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            webView = (WebView)rootView.findViewById(R.id.webView);
-            progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
-            progressBar.setVisibility(View.GONE);
-            webView = (WebView) rootView.findViewById(R.id.webView);
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.addJavascriptInterface(new Object() {
-                @JavascriptInterface
-                public void setPrintLink(final String printLink) {
-                    log(PRINT, null, "PDF: %s", printLink);
-                    MainActivity.printLink = printLink;
-                }
-            }, "PRINTLINK");
-            webView.setWebViewClient(new WebViewClient() {
-
-                public WebResourceResponse shouldInterceptRequest(final WebView view, final String url) {
-                    try {
-                        final URL aUrl = new URL(url);
-                        if (!ignoredHosts.contains(aUrl.getHost())) {
-                            if (!allowedHosts.contains(aUrl.getHost())) {
-                                log(NETWORK, null, "Loading: %s", aUrl.getHost());
-                            }
-                            return super.shouldInterceptRequest(view, url);
-                        }
-                        return new WebResourceResponse("text/html", "utf-8", null);
-                    } catch (final MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-
-                public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
-                    if (url.startsWith("mailto:")) {
-                        startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse(url)));
-                        return true;
-                    }
-                    return false;
-                }
-
-                @Override
-                public void onPageFinished(final WebView view, final String url) {
-                    log(NETWORK, null, "Loaded: %s", url);
-                    webView.loadUrl("javascript:window.PRINTLINK.setPrintLink(jQuery('.btn-print:first').attr('href'));");
-                    webView.loadUrl("javascript:jQuery('.tips-carousel,#buy-ingredients,.side-bar-content,.adsense-ads,#footer').hide()");
-                    saveLastPage(url);
-                }
-            });
-            final Animation slideOutTop = AnimationUtils.loadAnimation(getActivity(), R.anim.abc_slide_out_top);
-            final Animation slideInTop = AnimationUtils.loadAnimation(getActivity(), R.anim.abc_slide_in_top);
-            webView.setWebChromeClient(new WebChromeClient() {
-                public void onProgressChanged(final WebView view, final int progress) {
-                    progressBar.setProgress(progress);
-                    if (progress == 100) {
-                        progressBar.startAnimation(slideOutTop);
-                        progressBar.setVisibility(View.GONE);
-                    } else if (progressBar.getVisibility() == View.GONE) {
-                        progressBar.startAnimation(slideInTop);
-                        progressBar.setVisibility(View.VISIBLE);
-                    }
-                }
-            });
-
-            downloadManager = (DownloadManager) getActivity().getSystemService(DOWNLOAD_SERVICE);
-            getActivity().registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-
-            loadLastPage();
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-
-        private void goHome() {
-            webView.loadUrl(goodFoodHome);
-        }
-
-        private void shareCurrentPage() {
-            final Intent i = new Intent(Intent.ACTION_SEND);
-            i.setType("text/plain");
-            i.putExtra(Intent.EXTRA_SUBJECT, webView.getTitle());
-            i.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
-            startActivity(Intent.createChooser(i, "Share - " + webView.getTitle()));
-        }
-
-        private void saveLastPage(final String url) {
-            log(PREFS, null, "Saving page [%s]", url);
-            getAppSharedPreferences().edit().putString("LastPage", url).apply();
-        }
-
-        private void loadLastPage() {
-            final String lastPage = getAppSharedPreferences().getString("LastPage", goodFoodHome);
-            log(PREFS, null, "Loading last page [%s]", lastPage);
-            webView.loadUrl(lastPage);
-        }
-
-        private SharedPreferences getAppSharedPreferences() {
-            return getActivity().getSharedPreferences("BF", Context.MODE_PRIVATE);
-        }
-
-        private Set<String> newHashSet(final String... entries) {
-            final HashSet<String> set = new HashSet<>();
-            Collections.addAll(set, entries);
-            return set;
-        }
-
-        private void startDownload(final boolean open) {
-            MainActivity.open = open;
-            if (!TextUtils.isEmpty(printLink)) {
-                final File betterFoodDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "BetterFood");
-                betterFoodDirectory.mkdirs();
-                final File download = new File(betterFoodDirectory, webView.getTitle() + ".pdf");
-                if (!download.exists()) {
-                    final Uri uri = Uri.parse(goodFoodHome + printLink);
-                    log(NETWORK, null, "Downloading [%s]", uri);
-                    lastDownload = downloadManager.enqueue(new DownloadManager.Request(uri)
-                            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
-                            .setAllowedOverRoaming(false)
-                            .setTitle(webView.getTitle())
-                            .setDestinationUri(Uri.parse(download.toURI().toString())));
-                } else if (open) {
-                    log(NETWORK, null, "Already downloaded [%s]", download);
-                    openPdf(Uri.parse(download.toURI().toString()));
-                } else {
-                    Toast.makeText(getActivity(), "File already downloaded to Downloads directory, click 'View downloads' to view it.",
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-
-        private void viewDownloads() {
-            startActivity(new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS));
-        }
-
-        private void openPdf(final Uri uri) {
-            log(NETWORK, null, "Opening [%s]", uri);
-            startActivity(new Intent(Intent.ACTION_VIEW).setDataAndType(uri, "application/pdf"));
-        }
-
-        private final BroadcastReceiver onComplete = new BroadcastReceiver() {
-            public void onReceive(final Context ctxt, final Intent intent) {
-                final Cursor c = downloadManager.query(new DownloadManager.Query().setFilterById(lastDownload));
-                c.moveToFirst();
-                final String localUri = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-                log(NETWORK, null, "Downloaded to [%s]", localUri);
-                final Uri uri = Uri.parse(localUri);
-                if (open) {
-                    openPdf(uri);
-                } else {
-                    Toast.makeText(getActivity(), "File downloaded to Downloads directory, click 'View downloads' to view it.",
-                            Toast.LENGTH_LONG).show();
-                }
-            }
-        };
     }
 }
