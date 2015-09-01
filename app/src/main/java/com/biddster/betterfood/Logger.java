9bed3db877a9d2509e3d96604a0b82268581374d
biddster <luke.biddell@gmail.com>
2015-09-01 23:56:18 +0100

    Beginnings of a timer class.

--- app/src/main/java/com/biddster/betterfood/Logger.java
+++ app/src/main/java/com/biddster/betterfood/Logger.java
@@ -2,16 +2,29 @@ package com.biddster.betterfood;
 
 import android.util.Log;
 
-final class Logger {
+public final class Logger {
 
     public static final String PREFS = "PREFS";
     public static final String PRINT = "PRINT";
     public static final String NETWORK = "NETWORK";
+    public static final String TIMER = "TIMER";
     public static final String UIEVENT = "UI Event";
     private static final String TAG_PREFIX = "BF|";
 
     private Logger() {
         // Static helper class, don't want any instances.
+    }
+
+    public static Timer startTimer(final String name) {
+        final Timer timer = new Timer();
+        timer.start = System.currentTimeMillis();
+        timer.name = name;
+        return timer;
+    }
+
+    public static void endTimer(final Timer timer) {
+        timer.end = System.currentTimeMillis();
+        log(TIMER, null, "[%s] elapsed [%d]ms", timer.name, timer.end - timer.start);
     }
 
     public static void log(final String tag, final Throwable t, final String format, final Object... args) {
@@ -22,5 +35,11 @@ final class Logger {
             msg.append((args != null && args.length > 0) ? String.format(format, args) : format);
             Log.d(TAG_PREFIX + tag, msg.toString(), t);
         }
+    }
+
+    public static class Timer {
+        private long start;
+        private long end;
+        private String name;
     }
 }
