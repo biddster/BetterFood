6156e30f2590065b6220113a97682edc8744a9f0
biddster <luke.biddell@gmail.com>
2015-09-03 00:04:40 +0100

    Proguard removal friendly log methods.

--- app/src/main/java/com/biddster/betterfood/Logger.java
+++ app/src/main/java/com/biddster/betterfood/Logger.java
@@ -17,7 +17,6 @@ public final class Logger {
 
     public static Timer startTimer(final String name) {
         final Timer timer = new Timer();
-        timer.start = System.currentTimeMillis();
         timer.name = name;
         return timer;
     }
@@ -27,18 +26,41 @@ public final class Logger {
         log(TIMER, null, "[%s] elapsed [%d]ms", timer.name, timer.end - timer.start);
     }
 
-    public static void log(final String tag, final Throwable t, final String format, final Object... args) {
+    public static void log(final String tag, final Throwable t, final String msg) {
         if (BuildConfig.DEBUG) {
-            final StringBuilder msg = new StringBuilder();
-            final StackTraceElement ste = Thread.currentThread().getStackTrace()[3];
-            msg.append(String.format("(%s:%d) | ", ste.getFileName(), ste.getLineNumber()));
-            msg.append((args != null && args.length > 0) ? String.format(format, args) : format);
-            Log.d(TAG_PREFIX + tag, msg.toString(), t);
+            logImpl(tag, t, msg);
         }
     }
 
+    public static void log(final String tag, final Throwable t, final String format, final Object arg1) {
+        if (BuildConfig.DEBUG) {
+            logImpl(tag, t, format, arg1);
+        }
+    }
+
+    public static void log(final String tag, final Throwable t, final String format, final Object arg1, final Object arg2) {
+        if (BuildConfig.DEBUG) {
+            logImpl(tag, t, format, arg1, arg2);
+        }
+    }
+
+    public static void logException(final Throwable t) {
+        // TODO - crashlytics etc.
+        if (BuildConfig.DEBUG) {
+            logImpl("EXCEPTION", t, t.getLocalizedMessage());
+        }
+    }
+
+    private static void logImpl(final String tag, final Throwable t, final String format, final Object... args) {
+        final StringBuilder msg = new StringBuilder();
+        final StackTraceElement ste = Thread.currentThread().getStackTrace()[4];
+        msg.append(String.format("(%s:%d) | ", ste.getFileName(), ste.getLineNumber()));
+        msg.append((args != null && args.length > 0) ? String.format(format, args) : format);
+        Log.d(TAG_PREFIX + tag, msg.toString(), t);
+    }
+
     public static class Timer {
-        private long start;
+        private final long start = System.currentTimeMillis();
         private long end;
         private String name;
     }
